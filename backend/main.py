95ba60728ce1981ef5d08156819dd530da3ebe8b
Ummar Faeeque <ummarfaeeque@Ummars-MacBook-Air.local>
2026-05-16 18:49:38 +0530

    Enable global CORS support

--- backend/main.py
+++ backend/main.py
@@ -3,27 +3,29 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app.services.evolution_pipeline import EvolutionPipeline
 
-app = FastAPI(title="EvoStack API")
+app = FastAPI()
 
+
+# VERY IMPORTANT
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "*"
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.get("/")
-def health():
+async def root():
     return {
         "status": "healthy",
         "service": "EvoStack API"
     }
 
+
 @app.post("/analyze")
-def analyze_repository(repo_url: str):
+async def analyze(repo_url: str):
 
     pipeline = EvolutionPipeline()
 
